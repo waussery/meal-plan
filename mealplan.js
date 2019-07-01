01253c9968a62ecfa2231bcca4b079a1adc09ed0
waussery <50677814+waussery@users.noreply.github.com>
2019-07-01 13:03:10 -0500

    Write first pass

---
+++ mealplan.js
@@ -0,0 +1,39 @@
+//List Food Choices
+const entrees = ['Salmon', 'Herb Chicken', 'Pork Chops'];
+const vegetables = ['Butter Thyme Peas', 'Salad', 'Brown Sugar Carrots', 'Roasted Mixed Vegetables'];
+const carbs = ['Mac and Cheese', 'Mashed Potatoes', 'Rice', 'Sweet Potatoes', 'Roasted Squash'];
+
+//Declare Meals
+var dinnerEntrees = [];
+var dinnerSides = [];
+var dinnerCarbs = [];
+const dinnerMeal = [dinnerEntrees, dinnerSides, dinnerCarbs];
+
+//Other Variables
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+//Define Meal Picker
+const chooseMeal = (type, choices) => {
+    for (let days = 0; days < 7; days++) {
+        let randNum = Math.floor(Math.random() * choices.length);
+        type.push(choices[randNum]);
+        //console.log(choices.length);
+        //console.log(randNum);
+    }
+}
+
+//Generate Meals
+chooseMeal(dinnerEntrees, entrees);
+chooseMeal(dinnerSides, vegetables);
+chooseMeal(dinnerCarbs, carbs);
+
+//List Meals
+console.log('Meals for the Week');
+console.log("");
+for (i = 0; i < 7; i++) {
+    console.log(weekDays[i] + ':');
+    console.log(dinnerEntrees[i])
+    console.log(dinnerSides[i])
+    console.log(dinnerCarbs[i])
+    console.log("");
+};
