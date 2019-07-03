f324134e08e5c56ec6a5d1f4c1d5736d62262e21
송서연 <southkorea@protonmail.com>
2019-07-04 08:02:18 +0900

    hm2
    The `createMeals` function creates an output string, then loops
    over all the days defined and for each day pick a random food
    item from each section.
    
    The output uses template literals
    This allows for a template to be made and then filled in with variables later
    ```js
    const variable = "carrots";
    console.log(`I love ${variable}!`) // I love carrots!
    ```
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
    
    for loop as been changed to `of`, you should use variables you have
    instead of hard coding numbers when you can, instead of `weeksDays[i]`
    you now just have `day` as you never use the index anyway.  
    
    `of` loops over all the values of an array while `in` loops over the index.
    
    Each choice is now chosen and appended instantly as opposed to generating
    each array of chosen and then looping over those chosen at the end.
    
    `\n` is a line break, it will create a new line and is useful for
    single-line templates creating multi-line responses

--- mealplan.js
+++ mealplan.js
@@ -1,39 +1,22 @@
 //List Food Choices
-const entrees = ['Salmon', 'Herb Chicken', 'Pork Chops'];
+const entrees    = ['Salmon', 'Herb Chicken', 'Pork Chops'];
 const vegetables = ['Butter Thyme Peas', 'Salad', 'Brown Sugar Carrots', 'Roasted Mixed Vegetables'];
-const carbs = ['Mac and Cheese', 'Mashed Potatoes', 'Rice', 'Sweet Potatoes', 'Roasted Squash'];
-
-//Declare Meals
-const dinnerEntrees = [];
-const dinnerSides = [];
-const dinnerCarbs = [];
-const dinnerMeal = [dinnerEntrees, dinnerSides, dinnerCarbs];
+const carbs      = ['Mac and Cheese', 'Mashed Potatoes', 'Rice', 'Sweet Potatoes', 'Roasted Squash'];
 
 //Other Variables
-const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const days       = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-//Define Meal Picker
-function chooseMeal(type, choices) {
-    for (let days = 0; days < 7; days++) {
-        let randNum = Math.floor(Math.random() * choices.length);
-        type.push(choices[randNum]);
-        //console.log(choices.length);
-        //console.log(randNum);
-    }
+// Create meals
+function createMeals() {
+	let output = "";
+	for (let day of days) {
+		const entree = entrees[Math.floor(Math.random() * entrees.length)];
+		const side   = vegetables[Math.floor(Math.random() * vegetables.length)];
+		const carb   = carbs[Math.floor(Math.random() * carbs.length)];
+		output += `${day}:\n${entree}, ${side}, ${carb}\n\n`;
+	}
+	return output;
 }
 
-//Generate Meals
-chooseMeal(dinnerEntrees, entrees);
-chooseMeal(dinnerSides, vegetables);
-chooseMeal(dinnerCarbs, carbs);
-
-//List Meals
-console.log('Meals for the Week');
-console.log("");
-for (let i = 0; i < 7; i++) {
-    console.log(weekDays[i] + ':');
-    console.log(dinnerEntrees[i]);
-    console.log(dinnerSides[i]);
-    console.log(dinnerCarbs[i]);
-    console.log("");
-}
+console.log('Meals for the Week:\n');
+console.log(createMeals())
