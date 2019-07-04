cde781d4094c97630ab335dfde7361322a7cfa57
Rubber Ducky <william.a.ussery@gmail.com>
2019-07-04 15:24:44 -0500

    Update meal picker function to both select and output choices. Remove superfluous variables. Consolidate for readability. Update loop in Generate Meals section from for-in to for-of. Also make the first line of output yellow, because yellow is pleasant.

--- mealplan.js
+++ mealplan.js
@@ -1,39 +1,21 @@
-//List Food Choices
+//List Variables
 const entrees = ['Salmon', 'Herb Chicken', 'Pork Chops', 'Barbecue'];
 const vegetables = ['Butter Thyme Peas', 'Salad', 'Brown Sugar Carrots', 'Roasted Zucchini', 'Roasted Mixed Vegetables', 'Steamed Broccoli', 'Cooked Greens'];
 const carbs = ['Quinoa', 'Mac and Cheese', 'Mashed Potatoes', 'Rice', 'Sweet Potatoes', 'Roasted Squash', 'Rice Pilaf', 'Beans'];
-
-//Declare Meals
-const dinnerEntrees = [];
-const dinnerSides = [];
-const dinnerCarbs = [];
-const dinnerMeal = [dinnerEntrees, dinnerSides, dinnerCarbs];
-
-//Other Variables
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 //Define Meal Picker
-function chooseMeal(type, choices) {
+function chooseMeal(choices) {
     let randNum = Math.floor(Math.random() * choices.length);
-    type.push(choices[randNum]);
-    //console.log(choices.length);
-    //console.log(randNum);
+    console.log(choices[randNum]);
 }
 
 //Generate Meals
-for (let days = 0; days < 7; days++) {
-    chooseMeal(dinnerEntrees, entrees);
-    chooseMeal(dinnerSides, vegetables);
-    chooseMeal(dinnerCarbs, carbs);
-}
-
-//List Meals
-console.log('Meals for the Week');
-console.log("");
-for (let i = 0; i < 7; i++) {
-    console.log(weekDays[i] + ':');
-    console.log(dinnerEntrees[i]);
-    console.log(dinnerSides[i]);
-    console.log(dinnerCarbs[i]);
-    console.log("");
+console.log('%cMeals for the Week:', 'color: yellow');
+for (day of weekDays) {
+    console.log(day + ':');
+    chooseMeal(entrees);
+    chooseMeal(vegetables);
+    chooseMeal(carbs);
+    console.log('');
 }
