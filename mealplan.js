adb42d0028acf02227d09b26157dff1115ef95f1
Rubber Ducky <william.a.ussery@gmail.com>
2019-07-03 21:52:09 -0500

    Relocate FOR loop from being part of the chooseMeal function to iterate through the Meal Generation stage

--- mealplan.js
+++ mealplan.js
@@ -14,18 +14,18 @@ const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 
 //Define Meal Picker
 function chooseMeal(type, choices) {
-    for (let days = 0; days < 7; days++) {
-        let randNum = Math.floor(Math.random() * choices.length);
-        type.push(choices[randNum]);
-        //console.log(choices.length);
-        //console.log(randNum);
-    }
+    let randNum = Math.floor(Math.random() * choices.length);
+    type.push(choices[randNum]);
+    //console.log(choices.length);
+    //console.log(randNum);
 }
 
 //Generate Meals
-chooseMeal(dinnerEntrees, entrees);
-chooseMeal(dinnerSides, vegetables);
-chooseMeal(dinnerCarbs, carbs);
+for (let days = 0; days < 7; days++) {
+    chooseMeal(dinnerEntrees, entrees);
+    chooseMeal(dinnerSides, vegetables);
+    chooseMeal(dinnerCarbs, carbs);
+}
 
 //List Meals
 console.log('Meals for the Week');
